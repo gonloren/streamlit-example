b287d250f599bf6b8541e1fe8d1e3b470b19bc5c
gonloren <79266529+gonloren@users.noreply.github.com>
2023-01-10 15:36:47 -0300

    Add files via upload

---
+++ exStreamlit_1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+st.title("hello")
+st.sidebar.title(“World Happiness Index 2021:”)
+st.image("https://images.pexels.com/photos/573259/pexels-photo-573259.jpeg?cs=srgb&dl=pexels-matheus-bertelli-573259.jpg&fm=jpg", caption="World Happiness Dataset")
+
